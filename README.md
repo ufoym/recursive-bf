6342862d4e8bccf2d0525fb06266b59d293d03ab
Ming <a@ufoym.com>
2017-05-15 13:18:24 +0800

    New start from here

---
+++ README.md
@@ -0,0 +1,29 @@
+Recursive bilateral filtering (developed by Qingxiong Yang) is pretty fast compared with most edge-preserving filtering methods
+- computational complexity is linear in both input size and dimensionality:
+- takes about 43 ms to process a one megapixel color image (i7 1.8GHz & 4GB mem)
+- about 18x faster than *Fast high-dimensional filtering using the permutohedral lattice*
+- about 86x faster than *Gaussian kd-trees for fast high-dimensional filtering*
+
+## Results
+<table>
+<tr>
+<td><img src="https://cloud.githubusercontent.com/assets/2270240/26041579/7d7c034e-3960-11e7-9549-912685043e39.jpg" width="300px"><br/><p align="center">Original Image</p></td>
+<td><img src="https://cloud.githubusercontent.com/assets/2270240/26041586/8b4afb42-3960-11e7-9bd8-62bbb924f1e9.jpg" width="300px"><br/><p align="center">OpenCV's BF (896ms)</p></td>
+<td><img src="https://cloud.githubusercontent.com/assets/2270240/26041590/8d08c16c-3960-11e7-8a0c-95a77d6d9085.jpg" width="300px"><br/><p align="center">RecursiveBF (18ms)</p></td>
+</tr>
+<tr>
+<td></td>
+<td><img src="https://cloud.githubusercontent.com/assets/2270240/26041583/86ea7b22-3960-11e7-8ded-5109b76966ca.jpg" width="300px"><br/><p align="center">Gaussian Blur</p></td>
+<td><img src="https://cloud.githubusercontent.com/assets/2270240/26041584/88dfc9b4-3960-11e7-8c9d-2634eac098d0.jpg" width="300px"><br/><p align="center">Median Blur</p></td>
+</tr></table>
+
+For more details of the algorithm, please refer to the original paper
+
+    @inproceedings{yang2012recursive,
+        title={Recursive bilateral filtering},
+        author={Yang, Qingxiong},
+        booktitle={European Conference on Computer Vision},
+        pages={399--413},
+        year={2012},
+        organization={Springer}
+    }
---
+++ example/example.cpp
@@ -0,0 +1,59 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.hpp"
+#include "stb_image_write.hpp"
+#include "../include/rbf.hpp"
+#include <stdio.h>
+#include <time.h>
+
+
+class Timer {
+private:
+	unsigned long begTime;
+public:
+	void start() { begTime = clock(); }
+	float elapsedTime() { return float((unsigned long)clock() - begTime) / CLOCKS_PER_SEC; }
+};
+
+int main(int argc, char*argv[])
+{
+	if (argc != 5)
+	{
+		printf("Usage:\n");
+		printf("--------------------------------------------------------------------\n\n");
+		printf("*.exe: filename_out filename_in (only ppm image) \n");
+		printf("       sigma_spatial(e.g., 0.03) sigma_range(e.g., 0.1)\n\n");
+		printf("--------------------------------------------------------------------\n");
+		return(-1);
+	}
+	else
+	{
+		const int n = 100;
+		const char * filename_out = argv[1];
+		const char * filename_in = argv[2];
+		float sigma_spatial = static_cast<float>(atof(argv[3]));
+		float sigma_range = static_cast<float>(atof(argv[4]));
+
+		int width, height, channel;
+		unsigned char * img = stbi_load(filename_in, &width, &height, &channel, 0);
+		unsigned char * img_out = nullptr;
+		Timer timer;
+
+		timer.start();
+		for (int i = 0; i < n; ++i)
+			recursive_bf(img, img_out, sigma_spatial, sigma_range, width, height, channel);
+		printf("Internal Buffer: %2.5fsecs\n", timer.elapsedTime() / n);
+
+
+		float * buffer = new float[(width * height* channel + width * height
+									+ width * channel + width) * 2];
+		timer.start();
+		for (int i = 0; i < n; ++i)
+			recursive_bf(img, img_out, sigma_spatial, sigma_range, width, height, channel, buffer);
+		printf("External Buffer: %2.5fsecs\n", timer.elapsedTime() / n);
+		delete[] buffer;
+
+		stbi_write_bmp(filename_out, width, height, channel, img_out);
+		delete[] img;
+	}
+}
